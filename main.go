8f92d077c5101ededd5fd548770ddfff04ed3b2d
Рябов Сергей Олегович <RyabovSO@hq.corp.mos.ru>
2019-10-02 17:25:59 +0300

    first exercise

--- main.go
+++ main.go
@@ -5,7 +5,28 @@ import (
 	"os"
 	"strings"
 )
-	
+
+//echo1
+/*func main() {
+	var s, sep string
+	for i := 0; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}*/
+
+//echo2
+/*func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[0:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}*/
+
+//echo3
 func main() {
-	fmt.Println(strings.Join(os.Args[1:], " "))
+	fmt.Println(strings.Join(os.Args[0:], " "))
 }
